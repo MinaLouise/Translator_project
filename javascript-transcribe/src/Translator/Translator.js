b46b3af39f252a4780a8e9f520ffe9d905f60c54
Mina <msanford24@basecampcodingacademy.org>
2023-10-20 14:20:23 -0500

    fix css

--- javascript-transcribe/src/Translator/Translator.js
+++ javascript-transcribe/src/Translator/Translator.js
@@ -3,16 +3,49 @@ import './Translator.css';
 import { AiOutlineClose } from 'react-icons/ai';
 
 const Translator = () => {
-
     const [inputText, setInputText] = useState('');
     const [outputLang, setOutputLang] = useState('ar');
     const [outputText, setOutputText] = useState('');
     const [isTranslated, setIsTranslated] = useState(null);
 
-    const translate = () => {
-        const apiKey = '65b9c82f65msha9305bceff58afbp1aa4d1jsn2e3ba4f1e9a8'; // Replace with your API key
-        const baseUrl = 'https://microsoft-translator-text.p.rapidapi.com/translate';
+    const handleInputChange = (e) => {
+        setInputText(e.target.value);
     }
+
+    const translate = () => {
+    const apiKey = '65b9c82f65msha9305bceff58afbp1aa4d1jsn2e3ba4f1e9a8'; // Replace with your API key
+    const baseUrl = 'https://microsoft-translator-text.p.rapidapi.com/translate';
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': apiKey,
+            'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+        },
+        body: JSON.stringify([{ Text: inputText }])
+    };
+
+    fetch(`${baseUrl}?to=${outputLang}&api-version=3.0&profanityAction=NoAction&textType=plain`, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                setIsTranslated(false);
+                console.log("There's an error");
+                return;
+            }
+            setIsTranslated(true);
+            response.json()
+                .then(data => {
+                    const translatedText = data[0].translations[0].text;
+                    setOutputText(translatedText);
+                    console.log(translatedText);
+                });
+        })
+        .catch(err => {
+            setIsTranslated(false);
+            console.error(err);
+        });
+}
 
     const clearInput = () => {
         setInputText('');
@@ -29,9 +62,9 @@ const Translator = () => {
                     </div>
                     <form className="input-form">
                         <textarea
-                            className="text-box"
+                            className="text-box input-text"
                             placeholder="Enter text (any language)"
-                            onChange={e => setInputText(e.target.value)}
+                            onChange={handleInputChange}
                             value={inputText}
                         />
                         {inputText !== "" &&
@@ -46,33 +79,33 @@ const Translator = () => {
                     <div className="top-row">
                         <h2>Translation</h2>
                     </div>
-                    <div className="output-form">
+                    <form className="output-form">
                         <textarea
                             className="text-box output-text"
-                            readOnly
                             value={outputText}
+                            readOnly
                         />
-                    </div>
-                    <div className="output-controls">
-                        <button
-                            className="btn btn-translate"
-                            onClick={translate}
-                        >
-                            Translate
-                        </button>
-                        <select
-                            name="languages"
-                            id="languages"
-                            className="form-select form-select-sm"
-                            onChange={e => setOutputLang(e.target.value)}
-                        >
-                            <option value="ar">Arabic</option>
-                            <option value="en">English</option>
-                            <option value="fr">French</option>
-                            <option value="de">German</option>
-                            <option value="es">Spanish</option>
-                        </select>
-                    </div>
+                    </form>
+                </div>
+                <div className="controls">
+                    <button
+                        className="btn btn-translate"
+                        onClick={translate}
+                    >
+                        Translate
+                    </button>
+                    <select
+                        name="languages"
+                        id="languages"
+                        className="form-select form-select-sm"
+                        onChange={e => setOutputLang(e.target.value)}
+                    >
+                        <option value="ar">Arabic</option>
+                        <option value="en">English</option>
+                        <option value="fr">French</option>
+                        <option value="de">German</option>
+                        <option value="es">Spanish</option>
+                    </select>
                 </div>
             </div>
         </section>
